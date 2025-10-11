ea6f514b71afd2ef8771d7aee6c85b70255fade6
nico <janssnic@arcada.fi>
2025-10-11 18:32:26 +0300

    refresh endpoint post and delete. not working all the way yet

--- src/routes/refresh.js
+++ src/routes/refresh.js
@@ -8,6 +8,47 @@ require('dotenv').config()
 const router = express.Router()
 const prisma = new PrismaClient()
 
+// router.get('/', (req, res) => {
+//     res.send('Test endpoint is working!');
+// });
+router.use(authorize)
+router.post('/', async (req, res) => {
+    const { token, expires_at } = req.body
+    const userId = parseInt(req.authUser.sub, 10) //hämtar user id från JWT som är en sträng och konverterar till nummer
+    console.log(`Creating token for user ID: ${userId}`)
 
+    try {
+        const newToken = await prisma.refresh_tokens.create({
+            data: {
+                user_id: userId,
+                token: token,
+                expires_at: expires_at
+            }
+        })
+        console.log(`New board created with ID: ${newToken.id}`)
+
+        res.status(201).json({ msg: "new token" })
+    } catch (error) {
+        console.error("Error creating token:", error)
+        res.status(500).json({ error: "Failed to create token." })
+    }
+})
+
+router.delete('/', async (req, res) => {
+    const userId = parseInt(req.authUser.sub, 10) 
+    console.log(`Deleting tokens for user ID: ${userId}`)
+
+    try {
+        const deletedToken = await prisma.refresh_tokens.delete({
+            where: { user_id: userId }
+        })
+        console.log(`Deleted token for user ID: ${userId}`)
+
+        res.status(200).json({ msg: "token deleted" })
+    } catch (error) {
+        console.error("Error deleting token:", error)
+        res.status(500).json({ error: "Failed to delete token." })
+    }
+})
 
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const usersRouter = require('./routes/users')
 const boardsRouter = require('./routes/boards')
+const refreshRouter = require('./routes/refresh')
 const cors = require('cors')
 const bodyParser = require('body-parser');
 
@@ -14,6 +15,8 @@ app.use(bodyParser.json());
 app.use(cors())
 app.use('/users', usersRouter)
 app.use('/boards', boardsRouter)
+app.use('/refresh', refreshRouter)
+
 
 
 console.log(`Node.js ${process.version}`)
